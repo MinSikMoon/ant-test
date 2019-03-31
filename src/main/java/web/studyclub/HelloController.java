77b77977e30adc544740117b8f352fa3e75d4cb0
minsikmoon <msmoon.business@gmail.com>
2019-04-01 00:50:58 +0900

    parameter 던질수 있는 bye 메소드 생성

--- src/main/java/web/studyclub/HelloController.java
+++ src/main/java/web/studyclub/HelloController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -15,6 +16,11 @@ public class HelloController {
 	@RequestMapping("/hello.do")
 	public String hello(Model model) {
 		model.addAttribute("greeting", "안녕하세요");
+		return "hello";
+	}
+	@RequestMapping("/bye")
+	public String bye(Model model, @RequestParam(value="param", required=false)String param) {
+		model.addAttribute("greeting", (param == null ? "null" : param));
 		return "hello";
 	}
 
