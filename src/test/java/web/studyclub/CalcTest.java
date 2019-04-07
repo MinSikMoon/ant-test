8013d50b8a4d2601ee3d43794e0b5e7b0cd7ec05
minsikmoon <msmoon.business@gmail.com>
2019-04-08 00:31:46 +0900

    분리 1차

--- src/test/java/web/studyclub/CalcTest.java
+++ src/test/java/web/studyclub/CalcTest.java
@@ -15,18 +15,20 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 //메서드 실행후에 다음 테스트 실행 전에 컨텍스트 초기화 하려면 @DirtiesContext를 쓴다. 
 //클래스 별로 context를 재사용하기 싫다면
 //DirtiesContext(classMode = ClassMode.AFTER_CLASS)
-@DirtiesContext(classMode=ClassMode.AFTER_CLASS)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:mvc-quick-start.xml"})
+@ContextConfiguration(locations = { "classpath:test.xml" })
 public class CalcTest {
     @Autowired
     Calculator calculator;
+
     @Test
-    public void sumTest(){
+    public void sumTest() {
         assertEquals(calculator.sum(1, 2), 3);
     }
+
     @Test
-    public void sumTest6(){
+    public void sumTest6() {
         assertEquals(calculator.sum(4, 2), 6);
     }
 }
